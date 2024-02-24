8ab1b48fd95f1f49f048567e6686188d56dd8284
Kaiqueav <kaiquecerq66@outlook.com>
2024-02-23 22:58:12 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <header>
+        <img :src="caminhodaFoto" alt="">
+        <div class="links">
+            <h2>QUEM SOMOS</h2>
+            <h2><a href="">CONTATO</a></h2>   
+        </div>
+        <div>
+            <button class="bg-red-500">RIFA SOLIDÁRIA</button>
+        </div> 
+        <div>
+            <img :src="caminhodosEmojis" alt="Redes Sociais">
+        </div>
+    </header>
+
+    
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            caminhodaFoto: '/logo.png',
+            caminhodosEmojis: '/emojis.png'
+        }
+    }
+}
+
+</script>
+<style>
+    .links{
+        color: inherit;
+        text-decoration: none;
+        
+    }
+    header{ 
+        padding: 2px;
+        display: flex;
+        justify-content:center;
+        align-items: center;
+        flex-direction: ;
+    }
+
+</style>
